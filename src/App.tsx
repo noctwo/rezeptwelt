7098b0f061d47376d2cdef41426d82f2baf5e9e9
noctwo <147552138+noctwo@users.noreply.github.com>
2024-09-18 14:41:09 +0200

    feat: searchbar and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
 import Home from './Pages/Home/Home'
 
 function App() {
 
   return (
     <>
+      <Header />
       <Home />
+      <Footer />
     </>
   )
 }
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import Footer from "../../Components/Footer/Footer";
-import Header from "../../Components/Header/Header";
 import { supabaseClient } from "../../lib/supabaseClient";
 import "./Home.css"
 import { Recipes } from "../../Types/supabase-own-types";
@@ -9,28 +7,53 @@ import { Recipes } from "../../Types/supabase-own-types";
 const Home = () => {
 
     const [recipes, setRecipes] = useState<Recipes[]>();
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [noSearchResultText, setNoSearchResultText] = useState<string>("");
 
     useEffect(() => {
         const fetchRecipes = async () => {
             let selectQuery = supabaseClient.from("Recipes").select("*");
 
+            if(searchTerm){
+                selectQuery = selectQuery.ilike("name", `%${searchTerm}`)
+            }
+
             const result = await selectQuery;
+
+            if(result.data?.length === 0){
+                setNoSearchResultText("Die Suche war erfolglos...")
+            } else{
+                setNoSearchResultText("");
+            }
 
             if (result.error) {
                 console.error(result.error);
             } else {
                 setRecipes(result.data);
-                console.log(recipes)
             }
         }
         fetchRecipes();
-    }, []);
+        
+    }, [searchTerm]);
 
     return ( 
-    <>
-    <Header />
-    <Footer />
-    </> 
+    <main>
+        <div className="searchbar-container">
+            <input type="text" id="recipe-search-input" placeholder="Nach Rezepten suchen..." value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+        </div>
+        <p>{noSearchResultText}</p>
+        {!recipes? <p>Sorry, no recipes found.</p> :
+        <div className="show-recipe-container">
+    {recipes?.map((recipe) => (
+        <div className="recipe-card" key={recipe.id}>
+        <img src={`${recipe.img_url}`} />
+        <p>{recipe.name}</p>
+        
+        </div>
+    ))}
+    </div>
+    }
+    </main> 
     );
 }
 
