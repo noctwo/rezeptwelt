71cbd35343f857c9561c47ebd0e168cbd3eae6fd
noctwo <147552138+noctwo@users.noreply.github.com>
2024-10-14 11:56:40 +0200

    feat: login and global user

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
 import Home from './Pages/Home/Home'
 import Detail from './Pages/Detail/Detail'
+import LoginPage from './Pages/LoginPage/LoginPage'
+import { UserProvider } from './Context/UserContext'
+
 
 function App() {
 
   return (
+    <UserProvider>
     <BrowserRouter>
       <Header />
       <Routes>
+      <Route path="/login" element={<LoginPage/>}/>
       <Route path="/" element={<Home />} />
       <Route path="/recipe/:id" element ={<Detail />} />
       </Routes>
       <Footer />
     </BrowserRouter>
+    </UserProvider>
   )
 }
 
